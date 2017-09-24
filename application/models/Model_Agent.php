649bc20f8312655fad67c721b9f21639c23add2e
cleme <cleme@PCAINAUD>
2017-09-24 23:13:35 +0200

    creation du projet

---
+++ application/models/Model_Agent.php
@@ -0,0 +1,11 @@
+<?php
+class Model_Agent extends CI_Controller{
+    
+    public function GetAllAgents(){
+        
+      $sql = $this->db->query("select * from agent");
+      return $sql -> result();
+        
+        
+    }
+}
---
+++ application/models/Model_Formation.php
@@ -0,0 +1,11 @@
+<?php
+
+class Model_Formation extends CI_Model{
+    
+    public function GetAllFormationsByActivite($numero)
+    {
+        
+        $sql = $this->db->query("select * from formation where numeroActivite=".$numero);
+        return $sql->result();
+    }
+}
